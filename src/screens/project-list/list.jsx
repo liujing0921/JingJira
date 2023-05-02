e0329e16d1bada65247a181b120942eba405fdfe
刘婧 <1215623364@qq.com>
2023-05-02 10:47:12 +0800

    feat: 实现项目列表

---
+++ src/screens/project-list/list.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+export const List = ({ list, users }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>名称</th>
+          <th>负责人</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((project) => (
+          <tr key={project.id}>
+            <td>{project.name}</td>
+            {/* find还有可能返回undefined,undefined.name会报错 */}
+            <td>
+              {users.find((user) => user.id === project.personId)?.name ||
+                "未知"}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
